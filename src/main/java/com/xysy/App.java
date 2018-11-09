17e37ecdd07b5ef4825771ba9b1cc2eb39d40715
53315557@qq.com <53315557@qq.com>
2018-11-09 18:23:50 +0800

    add examples

--- src/main/java/com/xysy/App.java
+++ src/main/java/com/xysy/App.java
@@ -1,13 +1,26 @@
 package com.xysy;
 
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+import java.io.InputStream;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        String filePath="/pizhu.docx";
+        App instance= new App();
+        try {
+            InputStream is = instance.getClass().getResourceAsStream(filePath);
+            XWPFDocument xwpfDocument = new XWPFDocument(is);
+            XWPFWordExtractor xwpfWordExtractor = new XWPFWordExtractor(xwpfDocument);
+
+            System.out.println("debug");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
